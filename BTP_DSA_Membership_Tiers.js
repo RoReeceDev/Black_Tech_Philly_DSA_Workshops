161cdf0a08c7e79097aca86249d5c79b80c0d2b8
RoReeceDev <roreecedev@gmail.com>
2024-04-13 14:14:01 -0400

    membership tiers prompt

---
+++ BTP_DSA_Membership_Tiers.js
@@ -0,0 +1,20 @@
+//Prompt: Create an algorithm that checks if a guest is a memeber of the gym 
+// and what tier memebershipo they have. There are three(3) tiers: bronze, silver, gold
+
+//Input
+//Gym Membership tiers = bronze, silver, gold
+//Membership List = guest who are in the gym's system
+
+//Output 
+//If the guest is a member and what membership they have 
+
+//Algorithm/ PsudeoCode
+
+//Step 1a: Have a membership list (list if people/members in the gym)
+//Step 1b: Members whoa re on the list, have an assocaited membership tier
+//Step 2a: Deteremine if a guest is on the membership list by iterating/ looping through it 
+//Step 2b: If the guest's name IS on the list, return their tier
+//Step 2c: Else, the guest's name is not on the list, return not a member, they are a guest
+
+//Code Solution:
+
