81e3b6fe20216873eda5a4bcddc997617ceacf25
RoReeceDev <roreecedev@gmail.com>
2024-04-13 13:58:21 -0400

    Apples and Oranges

---
+++ BTP_DSA_Apples_Oranges.js
@@ -0,0 +1,22 @@
+//Prompt: Create an algorithm that will separate apples and oranges from a bag of unsorted mixed fruit 
+
+//Input
+
+//List unsorted fruits that include apples and oranges.
+
+//Output
+
+// A List that contained apples,
+// A list that contains oranges, 
+// A list that contains the unsorted remaining fruit 
+
+//Alogrithm
+
+//Step 1: Take the list of unsorted fruit and loop through it 
+//Step 2A: Check if the fruit has a value of apple. If it does, send it to the apples list.
+//Step 2B: Check if the fruit has a value of orange. If it does, send it to the oranges list.
+//Step 2C: If the fruit is neither apple or orange, send it to the unsorted list.
+
+
+//Code Solution
+
